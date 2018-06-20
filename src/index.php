9584f316d009e073c16aae1bee6fbfed671632ae
JimChen <18219111672@163.com>
2018-06-20 23:39:01 +0800

    init project

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 6/20/2018
+ * Time: 11:24 PM
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
